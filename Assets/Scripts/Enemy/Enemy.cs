b5248cc229926601329fec5b7028587d111fadfc
Heph0 <asanchez2871@gmail.com>
2021-11-11 20:09:42 -0600

    Spawn Enemies #3
    Initial setup

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Animator animator;
+    private Rigidbody rb;
+
+    private string animationPath = "";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        setData();
+    }
+    
+    //sets the data when the enemy starts
+    public void setData()
+    { 
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+
+    public void Move()
     {
         
     }
